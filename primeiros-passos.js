42f72cc1251ae074ca4df3f2c5df9728ab50cd0d
Italo Andrade <italoo.andrade@icloud.com>
2026-06-22 19:20:29 -0300

    Liberar modulo primeiros passos

--- primeiros-passos.js
+++ primeiros-passos.js
@@ -6,3 +6,28 @@ btnStartJourney.addEventListener("click", () => {
     alert("Aqui amanhã vamos abrir a Carta 1 - O Mago");
 
 });
+
+const btnOuvirTexto = document.getElementById("btnOuvirTexto");
+const btnPararAudio = document.getElementById("btnPararAudio");
+
+btnOuvirTexto.addEventListener("click", () => {
+
+  speechSynthesis.cancel();
+
+  const texto = document.getElementById("textoApresentacao").innerText;
+
+  const fala = new SpeechSynthesisUtterance(texto);
+
+  fala.lang = "pt-BR";
+  fala.rate = 0.95;
+  fala.pitch = 1;
+
+  speechSynthesis.speak(fala);
+
+});
+
+btnPararAudio.addEventListener("click", () => {
+
+  speechSynthesis.cancel();
+
+});
